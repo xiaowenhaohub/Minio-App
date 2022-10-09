8b13f53f24ce00c66320f85cf56ebad07c60360a
xiaowenhao66 <571375739@qq.com>
2022-10-09 16:25:19 +0800

    feat: 启动类

---
+++ minio-admin/src/main/java/com/minio/MinioApplication.java
@@ -0,0 +1,17 @@
+package com.minio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author 小问号
+ * @date 2022/10/9 16
+ * description
+ */
+
+@SpringBootApplication
+public class MinioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MinioApplication.class, args);
+    }
+}
