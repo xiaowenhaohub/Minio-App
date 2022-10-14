a36faddd9553da1e34a337f84d50ea3662788fe6
xiaowenhao66 <571375739@qq.com>
2022-10-14 11:27:22 +0800

    banner

--- minio-admin/src/main/java/com/minio/MinioApplication.java
+++ minio-admin/src/main/java/com/minio/MinioApplication.java
@@ -1,8 +1,15 @@
 package com.minio;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author 小问号
@@ -12,7 +19,20 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class MinioApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MinioApplication.class, args);
+    private static final Logger log = LoggerFactory.getLogger(MinioApplication.class);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(MinioApplication.class, args);
+        Environment env = applicationContext.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        port = port == null ? "8080" : port;
+        String path = env.getProperty("server.servlet.context-path");
+        path = path == null ? "" : path;
+        log.info("\n----------------------------------------------------------\n\t" +
+                "项目启动成功! 访问路径:\n\t" +
+                "本地访问地址: \thttp://localhost:" + port + path + "/\n\t" +
+                "外部访问地址: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 }
