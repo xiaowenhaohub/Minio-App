e09c24fb9573254640e6620115329f061f9ae231
xiaowenhao66 <571375739@qq.com>
2022-10-21 12:03:23 +0800

    跨域配置

---
+++ minio-framework/src/main/java/com/minio/framework/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.minio.framework.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @ Author     ：小问号.
+ * @ Date       ：Created in 11:47 2022/10/21
+ * @ Modified By：
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowCredentials(true)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .maxAge(3600);
+    }
+
+}
