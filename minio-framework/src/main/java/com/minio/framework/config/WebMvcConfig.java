70eb8b01bbd26d0a9ecf21f515ceace1843dbe3a
xiaowenhao66 <571375739@qq.com>
2022-10-21 12:26:33 +0800

    webmvc

--- minio-framework/src/main/java/com/minio/framework/config/WebMvcConfig.java
+++ minio-framework/src/main/java/com/minio/framework/config/WebMvcConfig.java
@@ -14,10 +14,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
+        // 设置允许跨域的路径
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                // 设置允许跨域请求的域名
+                .allowedOriginPatterns("*")
+                // 是否允许cookie
                 .allowCredentials(true)
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                // 设置允许的请求方式
+                .allowedMethods("GET", "POST", "DELETE", "PUT")
+                // 设置允许的header属性
+                .allowedHeaders("*")
+                // 跨域允许时间
                 .maxAge(3600);
     }
 
